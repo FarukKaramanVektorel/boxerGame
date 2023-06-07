9591e358482ed3d3221cacd0ee753b0a751e1318
Faruk Karaman <134199695+FarukKaramanVektorel@users.noreply.github.com>
2023-06-07 20:59:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Boksor ahmet = new Boksor("Ahmet", 15, 100, 90, 60);
         Boksor ali = new Boksor("Ali", 10, 85, 90, 50);
-        Musabaka r = new Musabaka(marc, alex, 90, 100);
-        r.roundBasla();
+        Musabaka mac = new Musabaka(ahmet, ali, 90, 100);
+        mac.roundBasla();
     }
 }
